095b5d7c59ddc14387481b76af4d6f6bcc795120
flothepo <f.guthmann@mailbox.org>
2021-05-13 20:36:25 +0200

    emacs: cleanup packages.el

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,16 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
-;;; .doom.d/packages.el
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
 
+(package! adoc-mode)
 (package! company-box)
+(package! ediprolog)
 (package! flyspell-correct)
 (package! flyspell-correct-ivy)
-;;(package! format-all)
+(package! mixed-pitch)
 (package! org-superstar)
 (package! ox-twbs)
-(package! mixed-pitch)
-(package! ediprolog)
-(package! adoc-mode)
